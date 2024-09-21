3fbc899abb123f9736cc75c3d73a6f066d1ee76d
Daniel Eze <dan4allu93@gmail.com>
2024-09-21 12:33:24 +0100

    added the api request methods

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -10,4 +10,65 @@ import Foundation
 /// Object that represents a single API call
 final class RMRequest {
     
+    /// The API Constants
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    /// The desired endpoint
+    private let endpoint: RMEndpoint
+    /// The path components for API if any
+    /// e.g https://rickandmortyapi.com/api/1
+    private let pathComponents: Set<String>
+    /// The query parameters if we have any
+    private let queryParameters: [URLQueryItem]
+    
+    
+    /// Constructed url for the api in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            for pathComponent in pathComponents {
+                string += "/\(pathComponent)"
+            }
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            
+            let argumentString = queryParameters.compactMap({
+                return "\($0.name)=\($0.value!)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    /// The request url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    /// HTTP Method
+    public let httpMethod = "GET"
+    
+    /// Our public constructor
+    /// - Parameters:
+    ///   - endpoint: The endpoint
+    ///   - pathComponents: The path components
+    ///   - queryParameters: The query parameters
+    public init(
+        endpoint: RMEndpoint,
+        pathComponents: [String] = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -21,8 +21,13 @@ final class RMService {
     /// Send Rick and Morty API call
     /// - Parameters:
     ///   - request: Request instance
+    ///   - type: The type of object we expect to get back
     ///   - completion: Callback with data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute<T: Codable>(
+        _ request: RMRequest,
+        expecting: T.Type,
+        completion: @escaping (Result<String, Error>) -> Void
+    ) {
         
     }
 }
